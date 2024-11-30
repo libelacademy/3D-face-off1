7cd8dc55ce67484de844069a8cec7f4c6749e3b6
SEBASTIAN <soporte@libelstudios.com>
2024-11-29 20:31:08 -0500

    update url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://libel.academy/3D-face-off1',
+	base: 'https://libel.academy/3D-face-off',
 });
